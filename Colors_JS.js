fab3dd3a5b661d65516c4a269a2494949cb81fa2
UniverseNature <70004605+UniverseNature@users.noreply.github.com>
2020-11-07 12:00:54 +0530

    well?
    idk anything i feel stupid

---
+++ Colors_JS.js
@@ -0,0 +1,17 @@
+alert("Make sure your zoom level is 100%, or else the page will not show the way it's intended to.");
+
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#navbarNav").collapse('hide');
+    }
+  });
+
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
